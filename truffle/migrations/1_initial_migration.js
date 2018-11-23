7744175457022a1dcfd5d49bae58d14a0f9158ba
jk <327413510@qq.com>
2018-11-23 21:22:32 +0800

    修改migrations下的1_initial_migration.js文件，为编译做准备

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-var Migrations = artifacts.require("./Migrations.sol");
+//var Migrations = artifacts.require("./Migrations.sol");
+var Conference = artifacts.require("./Conference.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  //deployer.deploy(Migrations);
+  deployer.deploy(Conference);
 };
